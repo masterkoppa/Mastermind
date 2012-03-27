55dac5a78860af05a44789a490cd2c6c7fe56a84
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-03-26 21:02:37 -0400

    Add the Code class

---
+++ src/mastermind/core/Code.java
@@ -0,0 +1,48 @@
+package mastermind.core;
+
+public class Code {
+	
+	/**
+	 * Global variable that states the number of pegs available per code.
+	 * Stated here in case this needs to be changed in the future.
+	 */
+	public static final int NUM_OF_PEGS = 5;
+	
+	/**
+	 * Array that holds all the pegs that make this code
+	 */
+	private ColorPeg[] Pegs;
+	
+	/**
+	 * Initialize an empty code, all the color pegs are null at this point
+	 */
+	public Code(){
+		Pegs = new ColorPeg[NUM_OF_PEGS];
+	}
+	
+	/**
+	 * Returns an array of pegs that make up this code.
+	 * For empty spaces the array will have null in that spot.
+	 * @return ColorPeg Array with ColorPeg Objects representing the pegs, null for empty spaces
+	 */
+	public ColorPeg[] getPegs(){
+		return this.Pegs;
+	}
+	
+	/**
+	 * Allows you to set a specific color to a position in this code
+	 * @param ID The index in the array starting at 0.
+	 * @param color The color to be placed here
+	 */
+	public void setPegs(int ID, ColorPeg color){
+		
+		if(ID >= NUM_OF_PEGS) 
+			throw new IllegalArgumentException("The index for this peg exeeds the max number of pegs");
+		
+		
+		Pegs[ID] = color;
+		
+	}
+	
+
+}
