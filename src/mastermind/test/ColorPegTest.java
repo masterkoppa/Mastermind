baf95226ef75ced85896d9d744607f054a022731
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-03-26 20:47:05 -0400

    Add an example JUnit Test File for the enum class ColorPeg. Not completely necessary but mostly a proof of concept to refresh Unit testing.

---
+++ src/mastermind/test/ColorPegTest.java
@@ -0,0 +1,25 @@
+package mastermind.test;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+
+import mastermind.core.ColorPeg;
+
+
+import org.junit.Test;
+
+public class ColorPegTest {
+
+
+	@Test
+	public void testGetName() {
+		assert(ColorPeg.WHITE.getName().equalsIgnoreCase("White"));
+	}
+
+	@Test
+	public void testGetColor() {
+		assert(ColorPeg.BLACK.getColor().equals(Color.BLACK));
+	}
+
+}
