8bf76e9446bb24f5060eb8475f2a9db2d2a65927
Alex Kelley <ajk6045@rit.edu>
2012-03-31 15:47:44 -0400

    finished basic look of StartGUI

--- src/StartGUI.java
+++ src/StartGUI.java
@@ -6,14 +6,33 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import mastermind.gui.RoundButton;
+
 
 public class StartGUI extends JFrame {
-	private ArrayList<JButton> secretCode = new ArrayList<JButton>();
+	private ArrayList<RoundButton> secretCode = new ArrayList<RoundButton>();
+	private JButton start = new JButton("Start Game");
 	
 	public StartGUI(){
 		super();
 		JPanel codePanel = new JPanel();
-		codePanel.setLayout(new GridLayout(2, 1));
+		codePanel.setLayout(new FlowLayout());
+		
+		for(int i = 0; i < 4; i++){
+			RoundButton sTemp = new RoundButton("s");
+			codePanel.add(sTemp);
+			secretCode.add(sTemp);
+		}
+		
+		setLayout(new GridLayout(2,1));
+		add(codePanel);
+		add(start);
 		
 	}
+	
+	public static void main(String[] args){
+		StartGUI gui = new StartGUI();
+		gui.setVisible(true);
+	}
+	
 }
