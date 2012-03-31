a3e0afcaedec1f8424d2f9549a100c0cb743b222
Andrew Church <ahc2483@gmail.com>
2012-03-31 14:09:29 -0400

    Removed testing view, added SubmitGuessCommand

---
+++ src/mastermind/core/commands/SubmitGuessCommand.java
@@ -0,0 +1,18 @@
+package mastermind.core.commands;
+
+public class SubmitGuessCommand implements ICommand {
+
+	public SubmitGuessCommand(GuessList listOfGuesses, Color[])
+	
+	public void execute() 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void undo()
+	{
+		
+	}
+
+}
