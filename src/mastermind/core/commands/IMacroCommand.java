e048132c361bc54e2482088f4c6b9aa55e5833d7
Andrew Church <ahc2483@gmail.com>
2012-03-31 16:22:06 -0400

    implemented PlayCommand

--- src/mastermind/core/commands/IMacroCommand.java
+++ src/mastermind/core/commands/IMacroCommand.java
@@ -2,11 +2,15 @@ package mastermind.core.commands;
 
 public interface IMacroCommand {
 
+	//Adds a new command to the bottom of the list of commands
 	void add(ICommand command);
 	
+	//Removes the last command in the stack
 	void remove();
 	
+	//Iterates through all commands in order and executes each
 	void execute();
 	
+	//Iterates through the stack in reverse order and calls undo on each
 	void undo();
 }
