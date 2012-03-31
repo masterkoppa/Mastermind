d45bf1b0723f605245ab93be85dad54305b57617
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-03-31 18:51:18 -0400

    Add some helper methods to the feedback peg class. They'll become usefull when working with GUI stuff.

--- src/mastermind/core/FeedbackPeg.java
+++ src/mastermind/core/FeedbackPeg.java
@@ -1,10 +1,27 @@
 package mastermind.core;
 
+import java.awt.Color;
+
 public enum FeedbackPeg {
 	
-	BLACK,
-	WHITE;
+	BLACK("Black", Color.BLACK),
+	WHITE("White", Color.WHITE);
 	
 	//TODO: Add color codes and possibly a description of each peg, for logging purposes?
-
+	
+	private String name;
+	private Color color;
+	
+	FeedbackPeg(String name, Color color){
+		this.name = name;
+		this.color = color;
+	}
+	
+	public Color getColor(){
+		return this.color;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
 }
