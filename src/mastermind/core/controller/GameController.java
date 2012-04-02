6ddbbf101b029889421c207d2e9803dff47b5659
mattaddy <mxa5942@rit.edu>
2012-04-02 10:14:13 -0400

    Complete game controller with unit tests

--- src/mastermind/core/controller/GameController.java
+++ src/mastermind/core/controller/GameController.java
@@ -13,18 +13,30 @@ public class GameController implements IGameController {
 	}
 
 	public void executeCommand(ICommand command) {
+		trimHistory();
 		command.execute();
 		history.add(command);
 		nextUndo++;
 	}
 	
 	public void undoCommand() {
-		if (nextUndo <= 0) {
-			return;
+		// NOTE: We're using nextUndo - 1 as as index into the history, since
+		// our nextUndo counter is at 1 when one element gets added
+		if (nextUndo >= 0) {
+			ICommand command = history.get(nextUndo - 1);
+			command.undo();
+			nextUndo--;
 		}
-		ICommand command = history.get(nextUndo);
-		command.undo();
-		nextUndo--;
+	}
+	
+	public ArrayList<ICommand> getHistory() {
+		return history;
+	}
+	
+	private void trimHistory() {
+		for( int i = nextUndo; i < history.size(); i++ ) {
+			history.remove(i);
+		}
 	}
 	
 }
