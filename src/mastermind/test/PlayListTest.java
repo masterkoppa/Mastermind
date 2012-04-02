5ef3f2efb2c0e8b7f75e28e6e6282b01184f9a84
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-04-01 22:52:21 -0400

    Add a file to enable the testing of the model and how the commands interract with it. First step to fixing issue #5

---
+++ src/mastermind/test/PlayListTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package mastermind.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Andres J Ruiz(ajr2546@rit.edu)
+ *
+ */
+public class PlayListTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
