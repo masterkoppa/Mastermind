668029137b217ffb38c2ccaa22d72a5511d1e742
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-04-02 22:04:33 -0400

    Fix bug #5 again, it got thrown back here due to some wierd stuff in git

--- src/mastermind/core/commands/ProvideFeedbackCommand.java
+++ src/mastermind/core/commands/ProvideFeedbackCommand.java
@@ -22,8 +22,9 @@ public class ProvideFeedbackCommand extends PlayListCommand implements ICommand 
 		Code secret = new Code(this.colors);
 		Guess latestGuess = this.guesses.getLatestMove();
 		Feedback f = Feedback.analyze(secret, latestGuess.getCode());
-		latestGuess.setFeedback(f);
-		GameLog.getInstance().info("Feedback provided");
+		//latestGuess.setFeedback(f);
+		guesses.addFeedbackToLastGuess(f);
+		//GameLog.getInstance().info("Feedback provided");
 	}
 
 	/**
@@ -33,7 +34,7 @@ public class ProvideFeedbackCommand extends PlayListCommand implements ICommand 
 	{
 		Guess latestGuess = this.guesses.getLatestMove();
 		latestGuess.setFeedback(null);
-		GameLog.getInstance().info("Feedback undone");
+		//GameLog.getInstance().info("Feedback undone");
 	}
 
 }
