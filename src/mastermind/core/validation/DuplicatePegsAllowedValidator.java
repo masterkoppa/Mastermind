220587a2e116d69129beb813e20f9586396aa5c8
Andrew Church <ahc2483@gmail.com>
2012-04-21 15:12:05 -0400

    Added some comments

--- src/mastermind/core/validation/DuplicatePegsAllowedValidator.java
+++ src/mastermind/core/validation/DuplicatePegsAllowedValidator.java
@@ -3,6 +3,12 @@ package mastermind.core.validation;
 import mastermind.core.Code;
 import mastermind.core.ColorPeg;
 
+/**
+ * 
+ * @author Andrew Church
+ * Class that implements ICodeValidator to validate codes using basic logic
+ *
+ */
 public class DuplicatePegsAllowedValidator implements ICodeValidator {
 
 	@Override
