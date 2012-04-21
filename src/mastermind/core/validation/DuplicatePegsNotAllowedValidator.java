13e339dfb2348a11599dfa1144a521f78825c52f
Andrew Church <ahc2483@gmail.com>
2012-04-21 14:40:19 -0400

    Added code validation strategy and implemented allowing duplicate pegs

---
+++ src/mastermind/core/validation/DuplicatePegsNotAllowedValidator.java
@@ -0,0 +1,13 @@
+package mastermind.core.validation;
+
+import mastermind.core.Code;
+
+public class DuplicatePegsNotAllowedValidator implements ICodeValidator{
+
+	@Override
+	public Boolean validate(Code c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/mastermind/core/validation/ICodeValidator.java
@@ -0,0 +1,20 @@
+package mastermind.core.validation;
+import mastermind.core.Code;
+
+/**
+ * 
+ * @author Andrew Church
+ * 
+ * Public interface for code validation strategy
+ *
+ */
+public interface ICodeValidator {
+	
+	/**
+	 * Definition of validation method
+	 * @param c
+	 * @return whether the code is valid
+	 */
+	Boolean validate(Code c);
+
+}
