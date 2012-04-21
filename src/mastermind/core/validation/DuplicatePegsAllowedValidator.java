caba4f8b5144a291caa3e3ba1882da0286f06da3
Andrew Church <ahc2483@gmail.com>
2012-04-21 14:40:19 -0400

    Added code validation strategy and implemented allowing duplicate pegs

---
+++ src/mastermind/core/validation/DuplicatePegsAllowedValidator.java
@@ -0,0 +1,18 @@
+package mastermind.core.validation;
+
+import mastermind.core.Code;
+import mastermind.core.ColorPeg;
+
+public class DuplicatePegsAllowedValidator implements ICodeValidator {
+
+	@Override
+	public Boolean validate(Code c) {
+		ColorPeg[] pegs = c.getPegs();
+		
+		if(pegs.length < Code.NUM_OF_PEGS)
+			return false;
+		
+		return true;
+	}
+
+}
