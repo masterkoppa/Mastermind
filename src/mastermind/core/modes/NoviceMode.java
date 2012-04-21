515b06c6a1574123b500fe480f682e74b51640db
mattaddy <mxa5942@rit.edu>
2012-04-21 15:23:09 -0400

    Game modes (expert and novice)

---
+++ src/mastermind/core/modes/NoviceMode.java
@@ -0,0 +1,22 @@
+package mastermind.core.modes;
+
+import mastermind.core.validation.*;
+
+/**
+ * A novice game mode.
+ *
+ * @author Matt Addy <mxa5942@rit.edu>
+ *
+ */
+public class NoviceMode implements GameMode {
+	
+	/**
+	 * Get the secret code validator associated with this mode.
+	 * 
+	 * @return ICodeValidator a validator that does allow duplicate pegs.
+	 */
+	public ICodeValidator secretCodeValidator() {
+		return new DuplicatePegsAllowedValidator();
+	}
+
+}
