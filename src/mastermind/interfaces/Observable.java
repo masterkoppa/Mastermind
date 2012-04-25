bece27eaafde0fb57e11f3b80dc23fc880f47b7e
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2012-04-25 19:36:17 -0400

    Document the Observable Superclass
    Document the Observable Class with vivid details

--- src/mastermind/interfaces/Observable.java
+++ src/mastermind/interfaces/Observable.java
@@ -5,27 +5,56 @@ import java.util.List;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * An observable object is usually a model or class that will hold information
+ * that other classes might be interested in. Following the Observer pattern
+ * this class will notify every observer that wants information from it and is
+ * registered to it.
+ * 
+ * 
+ * @author Andres J Ruiz(ajr2546@rit.edu)
+ * 
+ */
 public abstract class Observable {
 
+	/**
+	 * All the registered observers, any classes that want to re-implement this
+	 * can use any kind of list the wish, if they wanted to change it for
+	 * whatever reason
+	 */
 	protected List<Observer> observers;
 
+	/**
+	 * Constructor that will initialize the list of observers
+	 */
 	protected Observable() {
 		observers = new ArrayList<Observer>();
 	}
 
+	/**
+	 * Register to be notified when this class changes
+	 * 
+	 * @param object
+	 *            Pass in a observer object that is interested in keeping track
+	 *            of information stored here
+	 */
 	public abstract void register(Observer object);
 
+	/**
+	 * Data has changed, this method will call the notify methods on all the
+	 * registered observers of this object
+	 */
 	protected void dataChanged() {
 		for (final Observer i : observers) {
-			SwingUtilities.invokeLater(new Runnable(){
+			SwingUtilities.invokeLater(new Runnable() {
 
 				@Override
 				public void run() {
 					i.notifyChange();
 				}
-				
+
 			});
-			
+
 		}
 	}
 }
